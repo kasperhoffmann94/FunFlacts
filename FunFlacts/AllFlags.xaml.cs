c89bc0b48576a7a77c68c0e6bde2803ed2dbdc02
Kasper Malmsiø Hoffmann <43535701+kasperhoffmann94@users.noreply.github.com>
2021-04-07 13:34:47 +0200

    Add project files.

---
+++ FunFlacts/AllFlags.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlagData;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FunFlacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlags : ContentPage
+    {
+        private FunFlactsViewModel flagsViewModel = DependencyService.Get<FunFlactsViewModel>();
+        private bool editMode;
+        public AllFlags()
+        {
+            InitializeComponent();
+            EditButtonProperties();
+            editMode = false;
+            FlagListView.ItemsSource = flagsViewModel.Flags;
+        }
+
+        private void FlagListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (editMode)
+            {
+                var flag = (sender as ListView).SelectedItem as Flag;
+                RemoveSelectedFlag(flag);
+            }
+            else
+            {
+                flagsViewModel.CurrentFlag = (sender as ListView).SelectedItem as Flag;
+                Navigation.PushAsync(new FlagDetailsPage());
+            }
+
+        }
+
+        private void RemoveSelectedFlag(Flag flag)
+        {
+            flagsViewModel.RemoveFlag(flag);
+        }
+        private void EditButtonProperties()
+        {
+            EditToolbar.Text = "Edit";
+            EditToolbar.IconImageSource = "ic_edit.png";
+
+        }
+
+        private void CancelEditButtonProperties()
+        {
+            EditToolbar.Text = "Cancel Edit";
+            EditToolbar.IconImageSource = "ic_cancel.png";
+        }
+        private void EditButton_OnClicked(object sender, EventArgs e)
+        {
+            editMode = !editMode;
+            if (editMode)
+            {
+                CancelEditButtonProperties();
+            }
+            else
+            {
+                EditButtonProperties();
+            }
+
+        }
+    }
+}
---
+++ FunFlacts/FlagDetailsPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using FlagData;
+using Xamarin.Forms;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+
+
+
+namespace FunFlacts
+{
+    public partial class FlagDetailsPage : ContentPage
+    {
+        private readonly FunFlactsViewModel vm;
+
+        public FlagDetailsPage()
+        {
+            vm = DependencyService.Get<FunFlactsViewModel>();
+     
+            InitializeComponent();
+           
+            BindingContext = vm;
+        }
+
+        private async void OnShow(object sender, EventArgs e)
+        {
+            vm.CurrentFlag.DateAdopted = vm.CurrentFlag.DateAdopted.AddYears(1);
+            await DisplayAlert(vm.CurrentFlag.Country,
+                $"{vm.CurrentFlag.DateAdopted:D} - {vm.CurrentFlag.IncludesShield}: {vm.CurrentFlag.MoreInformationUrl}", 
+                "OK");
+        }
+
+        private async void OnMoreInformation(object sender, EventArgs e)
+        {
+            //Device.OpenUri(vm.CurrentFlag.MoreInformationUrl);
+            await Launcher.OpenAsync(vm.CurrentFlag.MoreInformationUrl);
+        }
+
+        private void OnPrevious(object sender, EventArgs e)
+        {
+            vm.MoveToPreviousFlag();
+        }
+
+        private void OnNext(object sender, EventArgs e)
+        {
+            vm.MoveToNextFlag();
+        }
+    }
+}
